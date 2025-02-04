2c4f657f1d5c324a8ce22a8a042ad0adbd884e84
Aleksandra Tsyganok <shura.dvornikov003@gmail.com>
2025-02-04 10:37:35 +0000

    Clean comments

--- SystemInformaionScript.sh
+++ SystemInformaionScript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#GRADIENT=(31 33 32 36 34 35 91 93 96) #colors: red, yellow, green, turquoise, blue, violet, pink
 BLUE_BOLD='\033[1;46m'
 GREEN='\033[0;36m'
 
@@ -13,5 +12,4 @@ echo -e "${BLUE_BOLD}Distribute name:${NC} ${GREEN}$(lsb_release -d | cut -f2)${
 echo -e "${BLUE_BOLD}Number of cores:${NC} ${GREEN}$(nproc)${NC}"
 echo -e "${BLUE_BOLD}RAM size:${NC} ${GREEN}$(free -h | grep -E "Mem:" | awk '{print "Total:", $2, "Available:", $7}')${NC}"
 echo -e "${BLUE_BOLD}Hard disk capacity:${NC} ${GREEN}$(df -h)${NC}"
-#$ cat /proc/cpuinfo/ | grep processor wc -l 
-#total available
+
